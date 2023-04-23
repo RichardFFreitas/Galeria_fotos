6c953b4213f062358a4946fe7a53bb4982704607
Richard Freitas <richardfelipelives@gmail.com>
2023-04-22 22:19:42 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+   $('#botao-cancelar').click(function(){
+    $('form').slideUp();
+   })
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const enderecodaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecodaNovaImagem}"/>`).appendTo(novoItem);
+        $(`
+        <div class="overlay-imagem-link">
+        <a href="${enderecodaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+            Ver imagem em tamanho real
+        </a>
+        </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('')
+    })
+}) 
